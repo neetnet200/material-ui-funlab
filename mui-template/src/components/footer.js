bbfd843de985b5487ad328010a3a09bda1bbca8b
misskip <anita.desanmiguel@gmail.com>
2022-08-07 18:31:49 +1000

    add material-ui elements

--- mui-template/src/components/footer.js
+++ mui-template/src/components/footer.js
@@ -1,39 +1,77 @@
 import React from "react";
 import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import { Box, Grid, Typography, Divider } from "@mui/material";
-import { textAlign } from "@mui/system";
 
 function Footer() {
   return (
-    <Grid container xs={12}
-   >
-      <Grid item>
-        <Box>
-          <a href="https://www.linkedin.com/in/anitadesanmiguel">
-            <FaLinkedinIn color="black" />
-          </a>
-        </Box>
-        <Box>
-          <a href="https://mobile.twitter.com/neetnet200">
-            <FaTwitter color="black"/>
-          </a>
-        </Box>
-        <Box>
-          <a href="https://github.com/neetnet200">
-            <FaGithub color="black"></FaGithub>
-          </a>
-        </Box>
-      </Grid>
-      <Grid item>
-        <Box>
-          <Divider />
-          <Typography >
+    <Grid
+      container
+      spacing={5}
+      sx={{
+        mt: 10,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        p: 10,
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Grid item xs={3}>
+          <Box>
+            <a href="https://www.linkedin.com/in/anitadesanmiguel">
+              <FaLinkedinIn color="#ed6b87" style={{ fontSize: "32px" }} />
+            </a>
+          </Box>
+        </Grid>
+        <Grid item xs={3}>
+          <Box>
+            <a href="https://mobile.twitter.com/neetnet200">
+              <FaTwitter color="#ed6b87" style={{ fontSize: "32px" }} />
+            </a>
+          </Box>
+        </Grid>
+        <Grid item xs={3}>
+          <Box>
+            <a href="https://github.com/neetnet200">
+              <FaGithub color="#ed6b87" style={{ fontSize: "32px" }}></FaGithub>
+            </a>
+          </Box>
+        </Grid>
+      </Box>
+      <Grid
+        item
+        container
+        xs={12}
+        sx={{
+          mt: 1,
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Typography variant="caption" style={{textAlign: "center"}} color="#75b3aa">
             {" "}
             neetnet.io operates on the unceded land of the Wurundjeri peoples of
             the Kulin nation, we pay our respects to elders past present &
-            emerging
+            emerging @ 2022 neetnet.io All rights reserved
           </Typography>
-          <Typography>2022 neetnet.io All rights reserved</Typography>
         </Box>
       </Grid>
     </Grid>
--- mui-template/src/components/main.js
+++ mui-template/src/components/main.js
@@ -1,23 +1,48 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Typography, Button } from "@mui/material";
+import { Typography, Button, Grid, Box } from "@mui/material";
 
 function Main() {
   let navigate = useNavigate();
   return (
     <>
-      <Typography variant="h3">MATERIAL UI LAB</Typography>
-      <p>Experimenting with Material UI</p>
-
-      <Button
-        variant="contained"
-        color="success"
-        onClick={() => {
-          navigate("/funWithGrid");
+      <Grid container spacing={5}
+        sx={{
+          mt: 10,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          p: 10,
         }}
       >
-        Grid
-      </Button>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Grid item xs={12}>
+            <Box>
+              <Typography variant="h3">MATERIAL UI FUNLAB</Typography>
+            </Box>
+            <p>Experimenting with Material UI</p>
+          </Grid>
+          <Grid item xs={12}>
+            <Box>
+              <Button
+                variant="contained"
+                color="success"
+                onClick={() => {
+                  navigate("/funWithGrid");
+                }}
+              >
+                Grid
+              </Button>
+            </Box>
+          </Grid>
+        </Box>
+      </Grid>
     </>
   );
 }
